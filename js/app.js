2cb8362507994c3366f9539dd6e5559e6523860d
Tohirov Otabek <tohirovo630@gmail.com>
2022-09-23 00:46:10 +0500

    add author sign up

---
+++ js/app.js
@@ -0,0 +1,51 @@
+document.addEventListener("DOMContentLoaded", (e) => {
+  const page = location.pathname;
+  console.log(page);
+  if (page === "/index.html" || page === "/") {
+    console.log("salom");
+    getBooksRequest()
+      .then((data) => {
+        displayBooks(data.payload.docs);
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
+  }
+});
+
+function displayBooks(data) {
+  let result = "";
+  const bookMenuNode = document.querySelector(".books");
+  data.forEach((data) => {
+    result += `
+            <div class="book">
+            <img width="100%" src="./img/book.png" alt="Book Image" class="book_img" />
+            <h3>${data.title}</h3>
+            <h6>${data.author.firstName}</h6>
+            <img src="img/star.png" alt="Star" class="star" />
+            <h5>4.1 • ${data.pages} bet</h5>
+          </div>
+        `;
+  });
+  bookMenuNode.innerHTML = result;
+}
+
+const getBooksRequest = async () => {
+  try {
+    const response = await fetch("https://bookzone-v2.herokuapp.com/api/books");
+    if (!response.ok) {
+      if (response.status === 404) {
+        throw new Error("Iltimos to'g'ri manzilga so'rov jo'nating!");
+      } else if (response.status === 403) {
+        // displaying "hm, what about no?"
+      } else {
+        // displaying "dunno what happened \_(ツ)_/¯"
+      }
+      throw new Error(response);
+    }
+    let data = await response.json();
+    return data;
+  } catch (error) {
+    throw error;
+  }
+};
--- js/signup.js
+++ js/signup.js
@@ -1,36 +1,12 @@
-async function signUprRequest(formData) {
-    const req = await fetch ("https://bookzone-v2.herokuapp.com/api/sign-up", {
-        method: "POST",
-        headers:{
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formData),
-    });
-    const data = await req.json();
-    return data;
-}
-
-
-function SignUp(firstName, lastName, email, password, phone, lang, role) {
+document.addEventListener("DOMContentLoaded", (e) => {
+  const page = location.pathname;
+  console.log(page);
+  if (page === "/signup.html" || page === "/signup") {
+    const form = document.forms[0];
     try {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-        this.password = password;
-        this.phone = phone;
-        this.lang = lang;
-        this.role = role;
-    }
-    catch(err){
-        console.log(err);
-    }
-}
-
-const form = document.forms[0];
-try {
-    form.addEventListener("submit", (e)=> {
-        e.preventDefault();
-        const formData = new SignUp(
+        form.addEventListener("submit", (e) => {
+          e.preventDefault();
+          const formData = new SignUp(
             form.firstName.value,
             form.lastName.value,
             form.email.value,
@@ -38,13 +14,92 @@ try {
             form.phone.value,
             form.lang.value,
             form.role.value
-        );
-        console.log(formData);
-        signUprRequest(formData).then((data) => {
+          );
+          console.log(formData);
+          signUprRequest(formData).then((data) => {
             console.log(data);
-        })
-    })
-}
-catch (err){
+          });
+        });
+      } catch (err) {
+        console.log(err);
+    }
+    async function signUprRequest(formData) {
+        const req = await fetch("https://bookzone-v2.herokuapp.com/api/sign-up", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        });
+        const data = await req.json();
+        return data;
+      }   
+  }
+//   const author = document.querySelectorAll("input")
+  const form = document.forms[0];
+  if (page === "/signupauthor.html" || page === "/signupauthor") {
+    try {
+        form.addEventListener("submit", (e) => {
+          e.preventDefault();
+          const formData = new SignUpAuthor(
+            form.firstName.value,
+            form.lastName.value,
+            form.email.value,
+            form.password.value,
+            form.phone.value,
+            form.role.value,
+            form.date_of_birth.value,
+          );
+          console.log(formData);
+          signUprRequest(formData).then((data) => {
+            console.log(data);
+          });
+        });
+      } catch (err) {
+        console.log(err);
+    }
+    async function signUprRequest(formData) {
+        const req = await fetch("https://bookzone-v2.herokuapp.com/api/sign-up", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        });
+        const data = await req.json();
+        return data;
+      }
+
+  }
+});
+
+
+function SignUp(firstName, lastName, email, password, phone, lang, role) {
+  try {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.password = password;
+    this.phone = phone;
+    this.lang = lang;
+    this.role = role;
+  } catch (err) {
     console.log(err);
+  }
 }
+function SignUpAuthor(firstName, lastName, email, password, phone, role, date_of_birth) {
+    try {
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.email = email;
+      this.password = password;
+      this.phone = phone;
+      this.role = role || "author";
+      this.date_of_birth = date_of_birth;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+
+
